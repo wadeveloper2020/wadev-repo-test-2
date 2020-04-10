c1048b3065ebfc7e50fc0c8ba6054e4ab0e2a30b
wadeveloper2020 <wa.dev2020@gmail.com>
2020-04-10 21:55:11 +0700

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Remote</title>
+</head>
+<body>
+    <h3>Hello World!</h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta facilis quos ut vero eius. Maiores id tempore officiis! Voluptas adipisci ipsa quaerat odio omnis hic repellat nostrum sunt voluptate sed!</p>
+</body>
+</html>
